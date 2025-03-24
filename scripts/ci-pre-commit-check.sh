f76401980f4f8a65eb8b118e370bcbc26523fd9f
shirohasuki <whmio0115@gmail.com>
2025-03-24 17:58:30 +0800

    [CI] add pre-commit check.

--- scripts/ci-pre-commit-check.sh
+++ scripts/ci-pre-commit-check.sh
@@ -6,12 +6,23 @@ if [ ! -f "$PROOF_FILE" ]; then
   echo "Error: pre-commit proof file not found!"
   exit 1
 fi
-CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
-CURRENT_FILE_HASH=$(git diff HEAD^ HEAD | sha256sum | cut -d ' ' -f1)
-STORED_HASH=$(cat $PROOF_FILE)
-STORED_COMMIT_HASH=$(echo $STORED_HASH | cut -d ':' -f1)
-STORED_FILE_HASH=$(echo $STORED_HASH | cut -d ':' -f2)
-if [ "$CURRENT_COMMIT_HASH" != "$STORED_COMMIT_HASH" ] || [ "$CURRENT_FILE_HASH" != "$STORED_FILE_HASH" ]; then
-  echo "Error: pre-commit SHA-256 hash mismatch!"
+COMMIT_HASH=$(git rev-parse HEAD)
+ACTUAL_CONTENT_HASH=$(git show $COMMIT_HASH --pretty=format:%b | grep -v "diff --git a/.pre-commit-proof" | sha256sum | cut -d ' ' -f1)
+EXPECTED_CONTENT_HASH=$(cat $PROOF_FILE)
+if [ "$ACTUAL_CONTENT_HASH" != "$EXPECTED_CONTENT_HASH" ]; then
+  echo "Error: file content hash mismatch, maybe local pre-commit not run or content modified!"
   exit 1
 fi
+# if [ ! -f "$PROOF_FILE" ]; then
+#   echo "Error: pre-commit proof file not found!"
+#   exit 1
+# fi
+# CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+# CURRENT_FILE_HASH=$(git diff HEAD^ HEAD | sha256sum | cut -d ' ' -f1)
+# STORED_HASH=$(cat $PROOF_FILE)
+# STORED_COMMIT_HASH=$(echo $STORED_HASH | cut -d ':' -f1)
+# STORED_FILE_HASH=$(echo $STORED_HASH | cut -d ':' -f2)
+# if [ "$CURRENT_COMMIT_HASH" != "$STORED_COMMIT_HASH" ] || [ "$CURRENT_FILE_HASH" != "$STORED_FILE_HASH" ]; then
+#   echo "Error: pre-commit SHA-256 hash mismatch!"
+#   exit 1
+# fi
