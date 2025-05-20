d6fb1e5df213efb0e609159b1a8949541cce1f02
shirohasuki <whmio0115@gmail.com>
2025-05-20 19:21:20 +0800

    [fix(script)] fix path in vcs script

--- voyager-test/scripts/build-vcs.sh
+++ voyager-test/scripts/build-vcs.sh
@@ -62,9 +62,11 @@ if [ -z "$CONFIG" ]; then
   help
 fi
 
-DEBUG_POSTFIX=""
+DASH_DEBUG_POSTFIX=""
+POINT_DEBUG_POSTFIX=""
 if [ "$debug" == "debug" ]; then
-  DEBUG_POSTFIX="-debug"
+  DASH_DEBUG_POSTFIX="-debug"
+  POINT_DEBUG_POSTFIX=".debug"
 fi
 
 CYDIR=$(git rev-parse --show-toplevel)
@@ -74,6 +76,6 @@ source ${CYDIR}/voyager-test/scripts/env-source.sh vcs
 cd ${CYDIR}/sims/vcs/ || { echo "Cannot enter the directory: ${CYDIR}/sims/vcs/"; exit 1; }
 make -j$j ${debug} CONFIG=${CONFIG} || { echo "[Build vcs Failed!]==================="; exit 1; }
 mkdir -p ${CYDIR}/voyager-test/output/vcs
-cp ${CYDIR}/sims/vcs/simv-chipyard.harness-${CONFIG}${DEBUG_POSTFIX} ${CYDIR}/voyager-test/output/vcs/
-cp ${CYDIR}/sims/vcs/simv-chipyard.harness-${CONFIG}${DEBUG_POSTFIX}.daidir/ ${CYDIR}/voyager-test/output/vcs/ -r
-# make print_vars
+cp ${CYDIR}/sims/vcs/simv-chipyard.harness-${CONFIG}${DASH_DEBUG_POSTFIX} ${CYDIR}/voyager-test/output/vcs/
+# cp ${CYDIR}/sims/vcs/simv-chipyard.harness-${CONFIG}${DEBUG_POSTFIX}.daidir/ ${CYDIR}/voyager-test/output/vcs/ -r
+cp ${CYDIR}/sims/vcs/generated-src/chipyard.harness.TestHarness.${CONFIG}${POINT_DEBUG_POSTFIX}/gen-collateral ${CYDIR}/voyager-test/output/vcs/ -r
