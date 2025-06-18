ad8379487b78c74df4db77142585aa4456a61980
shirohasuki <whmio0115@gmail.com>
2025-06-18 11:43:34 +0800

    [workload/buckyball] feat: add inline assembly workload

---
+++ voyager-test/src/workloads/npu/buckyball/CTest/buckyball.h
@@ -0,0 +1,91 @@
+#ifndef BUCKYBALL_H
+#define BUCKYBALL_H
+
+#include <stdint.h>
+
+// String macros (from xcustom.h)
+#define STR1(x) #x
+#ifndef STR
+#define STR(x) STR1(x)
+#endif
+
+#define CAT_(A, B) A##B
+#define CAT(A, B) CAT_(A, B)
+
+// Buckyball accelerator configuration
+#define DIM 16                    // Matrix dimension
+#define SPAD_ADDR_LEN 14         // Scratchpad address length in bits
+#define MEM_ADDR_LEN 32          // Memory address length in bits
+#define BANK_NUM 4               // Number of banks
+#define BANK_ROWS 4096           // Rows per bank
+#define SP_MATRICES ((BANK_NUM * BANK_ROWS) / DIM)  // Number of matrices in scratchpad
+
+// RoCC instruction encoding
+#define CUSTOM_3 0x7B            // Custom-3 opcode (0111 1011)
+
+// Buckyball function codes (funct7 field)
+#define BB_MVIN_FUNCT 24         // 0x18 - Move in function code
+#define BB_MVOUT_FUNCT 25        // 0x19 - Move out function code  
+#define BB_MUL_FUNCT 32          // 0x20 - Matrix multiply function code
+#define BB_FLUSH_FUNCT 7         // 0x07 - Flush function code
+
+// Data type for matrix elements
+typedef int8_t elem_t;
+
+// Buckyball RoCC instruction macro (following xcustom.h style)
+#define BUCKYBALL_INSTRUCTION_R_R(rs1, rs2, func7) \
+  { \
+    asm volatile( \
+        ".insn r " STR(CUSTOM_3) ", " STR(0x3) ", " STR(func7) ", x0, %0, %1" \
+        : \
+        : "r"(rs1), "r"(rs2)); \
+  }
+
+#define BUCKYBALL_INSTRUCTION_FLUSH(func7) \
+  { \
+    asm volatile( \
+        ".insn r " STR(CUSTOM_3) ", " STR(0x3) ", " STR(func7) ", x0, x0, x0" \
+        : \
+        :); \
+  }
+
+// Buckyball custom instructions using inline assembly
+
+// Move data from DRAM to scratchpad
+// mem_addr: DRAM address, sp_addr: scratchpad address, rows: number of rows to transfer
+#define bb_mvin(mem_addr, sp_addr, rows) \
+    do { \
+        uint64_t rs1_val = (uint64_t)(mem_addr); \
+        uint64_t rs2_val = ((rows) << SPAD_ADDR_LEN) | ((sp_addr) & ((1UL << SPAD_ADDR_LEN) - 1)); \
+        BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MVIN_FUNCT); \
+    } while(0)
+
+// Move data from scratchpad to DRAM
+// mem_addr: DRAM address, sp_addr: scratchpad address, rows: number of rows to transfer
+#define bb_mvout(mem_addr, sp_addr, rows) \
+    do { \
+        uint64_t rs1_val = (uint64_t)(mem_addr); \
+        uint64_t rs2_val = ((rows) << SPAD_ADDR_LEN) | ((sp_addr) & ((1UL << SPAD_ADDR_LEN) - 1)); \
+        BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MVOUT_FUNCT); \
+    } while(0)
+
+// Matrix multiplication with warp16 pattern
+// op1_addr: first operand scratchpad address, op2_addr: second operand scratchpad address  
+// wr_addr: write result scratchpad address, iter: number of iterations
+#define bb_mul_warp16(op1_addr, op2_addr, wr_addr, iter) \
+    do { \
+        uint64_t rs1_val = ((op2_addr) << SPAD_ADDR_LEN) | ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1)); \
+        uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN) | ((wr_addr) & ((1UL << SPAD_ADDR_LEN) - 1)); \
+        BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MUL_FUNCT); \
+    } while(0)
+
+// Flush accelerator
+#define bb_flush() \
+    BUCKYBALL_INSTRUCTION_FLUSH(BB_FLUSH_FUNCT)
+
+// Utility functions
+void print_matrix(const char* name, elem_t* matrix, int rows, int cols);
+void init_matrix(elem_t* matrix, int rows, int cols, int seed);
+int compare_matrices(elem_t* a, elem_t* b, int rows, int cols);
+
+#endif
