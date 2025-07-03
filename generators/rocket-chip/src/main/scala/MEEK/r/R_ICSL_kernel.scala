26d0dabdcf292fad35961321bd941c7e74c5bf78
bigwwwttt <1048333431@qq.com>
2025-07-03 15:30:42 +0800

    reduce dcache size of rocketMEEK and delete ICSL's SRAM

--- generators/rocket-chip/src/main/scala/MEEK/r/R_ICSL_kernel.scala
+++ generators/rocket-chip/src/main/scala/MEEK/r/R_ICSL_kernel.scala
@@ -333,16 +333,16 @@ class R_ICSL_kernel (val params: R_ICSLParams) extends Module with HasR_ICSLIO_k
   debug_perf_num_st                             := Mux(io.debug_perf_reset.asBool, 0.U, debug_perf_num_st + io.st_deq)
   debug_perf_num_ld                             := Mux(io.debug_perf_reset.asBool, 0.U, debug_perf_num_ld + io.ld_deq)
 
-  val u_channel                                  = Module(new GH_MemFIFO(FIFOParams (32, 50)))
-  val debug_L_timer                              = RegInit(0.U(64.W))
-  debug_L_timer                                 := Mux(fsm_state === fsm_nonchecking, 0.U, Mux(fsm_state === fsm_checking, debug_L_timer + 1.U, debug_L_timer))
-  u_channel.io.enq_valid                        := Mux((fsm_state === fsm_postchecking) && (fsm_state_delay === fsm_checking) && ((debug_perf_howmany_checkpoints & 0x1FF.U) === 0x00.U), true.B, false.B)
-  u_channel.io.enq_bits                         := debug_L_timer
-  val debug_perf_sel_delay                       = RegInit(0.U(4.W))
-  debug_perf_sel_delay                          := io.debug_perf_sel
-  u_channel.io.deq_ready                        := (io.debug_perf_sel === 14.U) && (debug_perf_sel_delay === 15.U)
+//   val u_channel                                  = Module(new GH_MemFIFO(FIFOParams (32, 50)))
+//   val debug_L_timer                              = RegInit(0.U(64.W))
+//   debug_L_timer                                 := Mux(fsm_state === fsm_nonchecking, 0.U, Mux(fsm_state === fsm_checking, debug_L_timer + 1.U, debug_L_timer))
+//   u_channel.io.enq_valid                        := Mux((fsm_state === fsm_postchecking) && (fsm_state_delay === fsm_checking) && ((debug_perf_howmany_checkpoints & 0x1FF.U) === 0x00.U), true.B, false.B)
+//   u_channel.io.enq_bits                         := debug_L_timer
+//   val debug_perf_sel_delay                       = RegInit(0.U(4.W))
+//   debug_perf_sel_delay                          := io.debug_perf_sel
+//   u_channel.io.deq_ready                        := (io.debug_perf_sel === 14.U) && (debug_perf_sel_delay === 15.U)
 
-  debug_L_timer_worest                          := Mux(io.debug_perf_reset.asBool, 0.U, Mux(debug_L_timer > debug_L_timer_worest, debug_L_timer, debug_L_timer_worest))
+//   debug_L_timer_worest                          := Mux(io.debug_perf_reset.asBool, 0.U, Mux(debug_L_timer > debug_L_timer_worest, debug_L_timer, debug_L_timer_worest))
 
 
   io.debug_perf_val                             := Mux(io.debug_perf_sel === 7.U, debug_perf_howmany_checkpoints, 
@@ -357,10 +357,8 @@ class R_ICSL_kernel (val params: R_ICSLParams) extends Module with HasR_ICSLIO_k
                                                    Mux(io.debug_perf_sel === 11.U, debug_L_timer_worest,
                                                    Mux(io.debug_perf_sel === 10.U, debug_perf_CPStrans,
                                                    Mux(io.debug_perf_sel === 12.U, debug_perf_exetime,
-                                                   Mux(io.debug_perf_sel === 13.U, debug_perf_num_ld,
-                                                   Mux(io.debug_perf_sel === 14.U, u_channel.io.deq_bits, 
-                                                   Mux(io.debug_perf_sel === 15.U, u_channel.io.deq_bits, 0.U
-                                                   )))))))))))))))
+                                                   Mux(io.debug_perf_sel === 13.U, debug_perf_num_ld, 0.U
+                                                   )))))))))))))
   
   
   io.checker_core_status                        := Mux(!io.if_correct_process.asBool, 3.U, 
