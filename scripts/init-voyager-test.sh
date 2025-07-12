942d32abf9f7b41c8297b4f1da79cc056a6eebde
shirohasuki <whmio0115@gmail.com>
2025-07-13 02:34:26 +0800

    [scripts] fix: add embench-iot init

--- scripts/init-voyager-test.sh
+++ scripts/init-voyager-test.sh
@@ -8,16 +8,6 @@ CYDIR=$(git rev-parse --show-toplevel)
 
 source ${CYDIR}/env.sh
 
-cd ${CYDIR}/voyager-test
-mkdir -p build && cd build 
-cmake ..
-make
-
-
-# install requirements for sardine
-pip install -r ${CYDIR}/voyager-test/scripts/sardine/requirements.txt
-sudo npm install -g allure-commandline # may need sudo, this is not suitable for all users, need be fixed later
-
 cd ${CYDIR}
 git submodule update --init voyager-test/src/workloads/embench/embench-iot
 
@@ -25,3 +15,14 @@ cd ${CYDIR}/voyager-test/src/workloads/embench/embench-iot
 git apply --ignore-whitespace ../embench.patch
 cd ..
 ./build.sh
+
+
+cd ${CYDIR}/voyager-test
+mkdir -p build && cd build 
+cmake ..
+make
+
+# install requirements for sardine
+pip install -r ${CYDIR}/voyager-test/scripts/sardine/requirements.txt
+sudo npm install -g allure-commandline # may need sudo, this is not suitable for all users, need be fixed later
+
