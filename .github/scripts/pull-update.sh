deadedd2efea39d3279dd11fe2e7235018f8894b
shirohasuki <whmio0115@gmail.com>
2025-07-14 04:03:17 +0800

    [scripts] fix: add parsec3 submodule install in pull-wirh-update..

--- .github/scripts/pull-update.sh
+++ .github/scripts/pull-update.sh
@@ -5,6 +5,8 @@ CYDIR=$(git rev-parse --show-toplevel)
 cd ${CYDIR}
 source ${CYDIR}/env.sh
 
-git submodule update --init voyager-test/src/workloads/embench/embench-iot
+# git submodule update --init voyager-test/src/workloads/embench/embench-iot
 
-bash ./scripts/init-voyager-test.sh
+# bash ./scripts/init-voyager-test.sh
+
+git submodule update --init voyager-test/src/workloads/parsec-benchmark
