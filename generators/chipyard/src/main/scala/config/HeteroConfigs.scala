556f35a2cc683207d2e91cd3ecd5ce4f654f7cf5
zhe jiang <zhejiang.uk@gmail.com>
2025-07-19 03:13:02 +0800

    [Update] Updated Firesim Configurations for TO.

--- generators/chipyard/src/main/scala/config/HeteroConfigs.scala
+++ generators/chipyard/src/main/scala/config/HeteroConfigs.scala
@@ -53,44 +53,20 @@ class GemminiPrefetchConfig extends Config(
 
 // will be replaced by VoyagerChipConfig
 class OurHeterSoCConfig extends Config(
-  new chipyard.config.WithTileFrequency(1000, Some(0)) ++
-  new chipyard.config.WithTileFrequency(1000, Some(1)) ++
-  new chipyard.config.WithTileFrequency(1000, Some(2)) ++
-  new chipyard.config.WithTileFrequency(1000, Some(3)) ++
-  new chipyard.config.WithTileFrequency(1000, Some(4)) ++
-  new chipyard.config.WithTileFrequency(1000, Some(5)) ++
-  // new chipyard.config.WithTileFrequency(1000, Some(0)) ++
-  // new chipyard.config.WithTileFrequency(500, Some(1)) ++
-  // new chipyard.config.WithTileFrequency(500, Some(2)) ++
-  // new chipyard.config.WithTileFrequency(500, Some(3)) ++
-  // new chipyard.config.WithTileFrequency(500, Some(4)) ++
-  // new chipyard.config.WithTileFrequency(500, Some(5)) ++
+  new chipyard.config.WithTileFrequency(100, Some(0)) ++
+  new chipyard.config.WithTileFrequency(100, Some(1)) ++
+  new chipyard.config.WithTileFrequency(100, Some(2)) ++
   new freechips.rocketchip.guardiancouncil.WithGuardianCouncilNodes++
-  // new barf.WithHellaCachePrefetcher(Seq(5), barf.SingleStridedPrefetcherParams()) ++   // strided prefetcher, sits in front of the L1D$, monitors core requests to prefetching into the L1D$
-  
-  // new freechips.rocketchip.rocket.WithNBigNpuCores(1, nMSHRs = 16) ++ //independent Rocket for gemmini: hartid 5, with non-blocking L1D$
-  // new chipyard.config.WithMultiRoCCNpu ++
-  // new chipyard.config.WithMultiRoCCGemmini(5)(gemmini.GemminiConfigs.defaultConfig) ++ // put gemmini on hart-5(rocket)
   new freechips.rocketchip.rocket.WithNBuckyBallCores(1) ++ //independent Rocket for gemmini: hartid 5, with non-blocking L1D$
   new chipyard.config.WithMultiRoCCBB ++
-  new chipyard.config.WithMultiRoCCBuckyBall(5)(buckyball.BuckyBallConfigs.defaultConfig) ++ // put gemmini on hart-5(rocket)
+  new chipyard.config.WithMultiRoCCBuckyBall(3)(buckyball.BuckyBallConfigs.defaultConfig) ++ // put gemmini on hart-5(rocket)
   
   new chipyard.config.WithMultiRoCCMEEK ++
-  new chipyard.config.WithMultiSingleRoCCGHE(0, 1, 2, 3, 4) ++ //put custom RoCC on hart0-4 for custom0 ISA extension ++
-  new freechips.rocketchip.subsystem.WithInclusiveCache(capacityKB = 256) ++ //256KB L2Cache
+  new chipyard.config.WithMultiSingleRoCCGHE(0, 1, 2) ++ //put custom RoCC on hart0-4 for custom0 ISA extension ++
+  new freechips.rocketchip.subsystem.WithInclusiveCache(capacityKB = 64) ++ //256KB L2Cache
   new chipyard.config.WithSystemBusWidth(128) ++
-  // new freechips.rocketchip.rocket.WithMEEKAsynchronousCDCs(
-  // AsynchronousCrossing().depth,
-  // AsynchronousCrossing().sourceSync) ++
-  // // Frequency specifications
-  // new chipyard.clocking.WithClockGroupsCombinedByName(("uncore",Seq("sbus", "mbus", "pbus", "fbus", "cbus", "obus", "implicit", "clock_tap"),Nil),
-  //                                                     ("boom",Seq("tile_0"),Nil),//大核
-  //                                                     ("rockettile",Seq("tile_1","tile_2","tile_3","tile_4","tile_5"),Nil)//meek小核
-  //                                                     // ("gemini",Seq("tile_5"),Nil)
-  //                                                     )++
-  //  Crossing specifications
   new freechips.rocketchip.rocket.WithMEEKCores(GH_GlobalParams.GH_NUM_CORES - 1) ++
-  new boom.meek.common.WithNLargeBooms(1) ++
+  new boom.meek.common.WithNMediumBooms(1) ++
   new peripheral.WithMyPeripheral(0x10050000, 0x1000) ++
   new chipyard.config.AbstractConfig
 )
