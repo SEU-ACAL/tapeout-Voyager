c66fabb155910c4efe7ab9556bc1a99e29bfd2b5
shirohasuki <whmio0115@gmail.com>
2025-07-31 13:57:17 +0800

    [peri-npu]: update the path of define file

--- tapeout/src/main/resources/npu/output_buffer_interface.v
+++ tapeout/src/main/resources/npu/output_buffer_interface.v
@@ -1,4 +1,4 @@
-`include "defines.v"
+`include "../gen-collateral/defines.v"
 // `include "../0-RTL/AXI_SLAVE/defines.v"
 
 module output_buffer_interface(
--- tapeout/src/main/resources/npu/weight_memory_interface_S.v
+++ tapeout/src/main/resources/npu/weight_memory_interface_S.v
@@ -1,4 +1,4 @@
-`include "defines.v"
+`include "../gen-collateral/defines.v"
 // `include "../0-RTL/AXI_SLAVE/defines.v"
 
 module weight_memory_interface_S(
